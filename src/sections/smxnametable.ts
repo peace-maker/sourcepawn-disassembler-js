45d79afbab22960852d9653196ce6b2a6220ced1
Peace-Maker <peace-maker@wcfan.de>
2019-01-10 17:09:15 +0100

    Compute all offsets to strings in .names table
    Be able to list them all easily.

--- src/sections/smxnametable.ts
+++ src/sections/smxnametable.ts
@@ -4,10 +4,12 @@ import { SmxSection } from './smxsection';
 
 export class SmxNameTable extends SmxSection {
   private names: { [index: number]: string };
+  private offsetsList: number[];
 
   public constructor(file: FileHeader, section: SectionEntry) {
     super(file, section);
     this.names = {};
+    this.offsetsList = [];
   }
 
   // Returns a string at a given index.
@@ -32,5 +34,24 @@ export class SmxNameTable extends SmxSection {
     const value = new TextDecoder('utf-8').decode(array.slice(index, index + length));
     this.names[index] = value;
     return value;
+  }
+
+  // Returns a list of all root indexes that map to strings.
+  get offsets(): number[] {
+    if (this.offsetsList.length === 0) {
+      this.computeStringOffsets();
+    }
+    return this.offsetsList;
+  }
+
+  private computeStringOffsets(): void {
+    let lastIndex = 0;
+    const array = new Uint8Array(this.file.sectionReader(this.header));
+    for (let i = 0; i < this.header.size; i++) {
+      if (array[i] === 0) {
+        this.offsetsList.push(lastIndex);
+        lastIndex = i + 1;
+      }
+    }
   }
 }
